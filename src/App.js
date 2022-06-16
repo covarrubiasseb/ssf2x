cb321871ee2d7e3ac3a633ca1a2731ecf244d0fd
unknown <covarrubias.seb@gmail.com>
2022-06-16 04:47:51 -0700

    Created styled table with Bootstrap CSS.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.gif';
 import './App.css';
 import React from 'react';
 import CalcRankings from './CalcRankings.js';
@@ -30,19 +29,31 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header ms-5 me-5">
           <div id="rankingTable">
-            <h1 className="Table-heading">Super Street Fighter II X Rankings</h1>
-            <ul>
-              {
-                this.state.rankings.map((player, idx) => {
-                  return (
-                    <li key={idx} className="List-item">{player.name} : {player.score}</li>
-                  );
-                })
-              }
-            </ul>
+            <h1 className="text-center mb-5">Super Street Fighter II X Rankings</h1>
+            <table className="table table-bordered border-dark border-opacity-10">
+              <thead>
+                <tr className="bg-gradient">
+                  <th scope="col">#</th>
+                  <th scope="col">Player</th>
+                  <th scope="col">Rating</th>
+                </tr>
+              </thead>
+              <tbody className="table-group-divider">
+                {
+                  this.state.rankings.map((player, idx) => {
+                    return (
+                      <tr key={idx} className="bg-gradient">
+                        <th scope="row">{idx+1}</th>
+                        <td>{player.name}</td>
+                        <td>{player.score}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </table>
           </div>
         </header>
       </div>
