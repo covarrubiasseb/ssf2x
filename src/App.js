67a293c751ff687be7140a1666110e690cf5f155
unknown <covarrubias.seb@gmail.com>
2022-06-17 17:36:53 -0700

    Created SVG bars for player win percent display.

--- src/App.js
+++ src/App.js
@@ -46,6 +46,11 @@ class App extends React.Component {
                 <stop offset="5%" stopColor="#00066" />
                 <stop offset="95%" stopColor="#6666FF" />
               </linearGradient>
+
+              <linearGradient id="winPercentGradient">
+                <stop offset="5%" stopColor="#000" />
+                <stop offset="95%" stopColor="#369867" />
+              </linearGradient>
             </defs>
           </svg>
           <div id="rankingTable">
@@ -65,14 +70,32 @@ class App extends React.Component {
               <tbody className="table-group-divider">
                 {
                   this.state.rankings.map((player, idx) => {
+                    let winPercent = Math.round(player.win / (player.win + player.loss) * 100);
                     return (
                       <tr key={idx}>
                         <th scope="row">{idx+1}</th>
-                        <td><img className="me-4 img-fluid" src={"./img/"+player.characterKey+".png"}/><em>{player.name}</em></td>
-                        <td className="win-loss"><em><div className="border border-dark bg-gradient mt-1 text-center">{player.win} - {player.loss}</div></em></td>
-                        <td className="win-percent"><div className="border border-dark bg-gradient mt-1 text-center"><em>{Math.round(player.win / (player.win + player.loss) * 100)}%</em></div></td>
                         <td>
-                          <svg width={this.state.windowWidth / 3} height='60px' className="border border-dark border-2 bg-dark bg-gradient">
+                          <img className="me-4 img-fluid" src={"./img/"+player.characterKey+".png"}/><em>{player.name}</em>
+                        </td>
+
+                        <td className="win-loss">
+                          <div className="border border-dark bg-gradient mt-1 text-center">
+                            <em>{player.win} - {player.loss}</em>
+                          </div>
+                        </td>
+
+                        <td className="win-percent">
+                          <svg width={this.state.windowWidth / 16} height='60px' className="border border-dark border-2 bg-gradient">
+                            <g className="bars">
+                              <rect stroke="#000" fill="url(#winPercentGradient)" height='60px'
+                              width={(this.state.windowWidth / 16) * (winPercent / 100)}></rect>
+                              <text x="20%" y="80%" fill="#DDD" transform="skewX(-10) scale(0.55 0.55)">{winPercent}%</text>
+                            </g>
+                          </svg>
+                        </td>
+
+                        <td>
+                          <svg width={this.state.windowWidth / 3} height='60px' className="border border-dark border-2 bg-gradient">
                             <g className="bars">
                               <rect stroke="#000" fill="url(#RatingBarGradient)"
                               width={(this.state.windowWidth / 3) * (player.score/ratingsBarCurve)} height='60px'></rect>
